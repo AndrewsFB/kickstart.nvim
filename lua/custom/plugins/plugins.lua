da02ef612d6295c7ed473ca4080e89cc32209ce2
Andrews <andrews.fb87@gmail.com>
2025-04-11 00:28:01 -0300

    The following plugins have been added:
    - mfussenegger/nvim-dap
    - jay-babu/mason-nvim-dap.nvim
    - rcarriga/nvim-dap-ui
    - nvim-neotest/nvim-nio
    
    Some shortcuts for using the debugging features have also been created.

--- lua/custom/plugins/plugins.lua
+++ lua/custom/plugins/plugins.lua
@@ -1,5 +1,7 @@
 return {
 
+  { 'rcarriga/nvim-dap-ui', dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' } },
+
   {
     'nvim-tree/nvim-tree.lua',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -55,5 +57,113 @@ return {
       'nvim-treesitter/nvim-treesitter',
       'nvim-tree/nvim-web-devicons',
     },
+  },
+
+  -- Configuração do nvim-dap para C# com netcoredbg
+  {
+    'williamboman/mason.nvim',
+    config = function()
+      require('mason').setup()
+    end,
+    dependencies = {
+      'williamboman/mason-lspconfig.nvim',
+      'mfussenegger/nvim-dap',
+    },
+  },
+
+  {
+    'jay-babu/mason-nvim-dap.nvim',
+    dependencies = {
+      'williamboman/mason.nvim',
+      'mfussenegger/nvim-dap',
+    },
+    config = function()
+      require('mason-nvim-dap').setup {
+        -- Você pode adicionar configurações específicas aqui, veja a documentação do plugin
+        handlers = {},
+      }
+    end,
+  },
+
+  -- Configuração manual do adapter coreclr (caso o mason-nvim-dap não funcione como esperado)
+  {
+    'mfussenegger/nvim-dap',
+    config = function()
+      local dap = require 'dap'
+      dap.adapters.coreclr = {
+        type = 'executable',
+        command = '/usr/local/bin/netcoredbg',
+        options = {
+          cwd = '${workspaceFolder}',
+        },
+      }
+
+      dap.configurations.cs = {
+        {
+          name = '.NET Core Launch (console)',
+          type = 'coreclr',
+          request = 'launch',
+          program = function()
+            return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+          end,
+          cwd = '${workspaceFolder}',
+          console = 'integratedTerminal',
+        },
+        {
+          name = '.NET Core Attach',
+          type = 'coreclr',
+          request = 'attach',
+          processId = require('dap.utils').pick_process,
+          cwd = '${workspaceFolder}',
+        },
+      }
+    end,
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+    },
+  },
+
+  -- Configuração da UI do nvim-dap (opcional, mas recomendado)
+  {
+    'rcarriga/nvim-dap-ui',
+    config = function()
+      require('dapui').setup()
+    end,
+    dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' }, -- Adicionando nvim-nio como dependência aqui também por precaução
+  },
+
+  -- Keymaps para o nvim-dap (adicione isso em outro arquivo de configuração de keymaps, se você tiver um separado)
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.*',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+  {
+    'nvim-telescope/telescope-media-files.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+  },
+  {
+    'folke/which-key.nvim',
+    config = function()
+      require('which-key').setup()
+    end,
+  },
+  {
+    'folke/noice.nvim',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
+    config = function()
+      require('noice').setup {
+        -- add any options here
+      }
+    end,
+  },
+  {
+    'MunifTanjim/nui.nvim',
+  },
+  {
+    'rcarriga/nvim-notify',
   },
 }
--- lua/custom/plugins/shortcuts.lua
+++ lua/custom/plugins/shortcuts.lua
@@ -5,4 +5,16 @@ return {
   vim.api.nvim_set_keymap('n', '<leader>z', ':AerialClose<CR>', { noremap = true, silent = true }),
   vim.api.nvim_set_keymap('n', '<leader>[', ':NvimTreeRefresh<CR>:NvimTreeOpen<CR>', { noremap = true, silent = true }),
   vim.api.nvim_set_keymap('n', '<leader>]', ':NvimTreeClose<CR>', { noremap = true, silent = true }),
+
+  -- Atallhos para nvim-dap-ui
+  vim.api.nvim_set_keymap('n', '<leader>db', ':DapToggleBreakpoint<CR>', { desc = 'Toggle Breakpoint', noremap = true, silent = true }),
+  vim.api.nvim_set_keymap('n', '<leader>dc', ':DapContinue<CR>', { desc = 'Continue', noremap = true, silent = true }),
+  vim.api.nvim_set_keymap('n', '<leader>di', ':DapStepInto<CR>', { desc = 'Step Into', noremap = true, silent = true }),
+  vim.api.nvim_set_keymap('n', '<leader>do', ':DapStepOver<CR>', { desc = 'Step Over', noremap = true, silent = true }),
+  vim.api.nvim_set_keymap('n', '<leader>dr', ':DapReplOpen<CR>', { desc = 'Open REPL', noremap = true, silent = true }),
+  vim.api.nvim_set_keymap('n', '<leader>dl', ':DapRunLast<CR>', { desc = 'Run Last Debug', noremap = true, silent = true }),
+  vim.api.nvim_set_keymap('n', '<leader>dt', ':DapTerminate<CR>', { desc = 'Terminate Debug', noremap = true, silent = true }),
+  vim.api.nvim_set_keymap('n', '<leader>du', ':DapuiToggle<CR>', { desc = 'Toggle Debug UI', noremap = true, silent = true }),
+  vim.api.nvim_set_keymap('n', '<leader>ds', ':DapStepOut<CR>', { desc = 'Step Out', noremap = true, silent = true }),
+  vim.api.nvim_set_keymap('n', '<leader>de', ':DapEval<CR>', { desc = 'Evaluate Expression', noremap = true, silent = true }),
 }
